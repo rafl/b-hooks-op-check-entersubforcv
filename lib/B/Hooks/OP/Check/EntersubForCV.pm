a091e9d0af6f111bcc176eb70b3a6ea49bfff47b
Florian Ragwitz <rafl@debian.org>
2008-10-16 09:25:30 +0200

    Add basic docs.

--- lib/B/Hooks/OP/Check/EntersubForCV.pm
+++ lib/B/Hooks/OP/Check/EntersubForCV.pm
@@ -37,3 +37,56 @@ sub unimport {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+B::Hooks::OP::Check::EntersubForCV - Invoke callbacks on construction of entersub OPs for certain CVs
+
+=head1 SYNOPSIS
+
+=head2 From Perl
+
+    sub foo {}
+
+    use B::Hooks::OP::Check::EntersubForCV
+        \&foo => sub { warn "entersub for foo() being compiled" };
+
+    foo(); # callback is invoked when this like is compiled
+
+    no B::Hooks::OP::Check::EntersubForCV \&foo;
+
+    foo(); # callback isn't invoked
+
+=head2 From C/XS
+
+    #include "hooks_op_check_entersubforcv.h"
+
+    STATIC OP *
+    my_callback (pTHX_ OP *op, CV *cv, void *user_data) {
+        /* ... */
+        return op;
+    }
+
+    hook_op_check_id id;
+
+    /* register callback */
+    id = hook_op_check_entersubforcv (cv, my_callback, NULL);
+
+    /* unregister */
+    hook_op_check_entersubforcv_remove (id);
+
+=head1 AUTHOR
+
+Florian Ragwitz E<lt>rafl@debian.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2008 Florian Ragwitz
+
+This module is free software.
+
+You may distribute this code under the same terms as Perl itself.
+
+=cut
