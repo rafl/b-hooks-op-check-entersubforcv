47c94e6f774c3dc29d43a44d5005ff533f829723
Florian Ragwitz <rafl@debian.org>
2009-11-02 00:19:31 +0100

    Update copyright statement.

--- lib/B/Hooks/OP/Check/EntersubForCV.pm
+++ lib/B/Hooks/OP/Check/EntersubForCV.pm
@@ -139,7 +139,7 @@ Florian Ragwitz E<lt>rafl@debian.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2008 Florian Ragwitz
+Copyright (c) 2008, 2009 Florian Ragwitz
 
 This module is free software.
 
